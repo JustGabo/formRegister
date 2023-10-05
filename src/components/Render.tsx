a9f4cf525c47471866908c2bb3a2b5ce24daa7ee
Gabriel Soriano <ovallegabriel123@gmail.com>
2023-10-05 02:16:26 -0400

    changing the main page with on mobile screen

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -4,7 +4,7 @@ import Image from '../image/imageBlack.jpg'
 const Render = () => {
   return (
     <div className='flex  items-center justify-center h-[100vh]'>
-    <div className='lg:border lg:shadow-2xl border-gray-300 rounded-lg items-center  lg:w-[60%] m-auto lg:px-10 py-2 gap-5 lg:grid lg:grid-cols-[60%,1fr]'>
+    <div className='lg:border lg:shadow-2xl border-gray-300 rounded-lg items-center  lg:w-[60%] m-auto w-[80%] lg:px-10 py-2 gap-5 lg:grid lg:grid-cols-[60%,1fr]'>
         <Data/>
         <img className='object-cover hidden lg:flex bg-red-50 h-[80%] w-[100%] rounded-md' src={Image} alt="" />
     </div>
